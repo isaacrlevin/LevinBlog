d5250d19a7033e427b4b13dd4cee792a9f317b93
Isaac Levin <8878502+isaacrlevin@users.noreply.github.com>
2020-04-19 15:06:48 -0700

    Update parse-commit.sh

--- scripts/parse-commit.sh
+++ scripts/parse-commit.sh
@@ -1,2 +1,4 @@
 IFS='|' read -ra my_array <<< $COMMIT
-echo "::set-env name=COMMIT_ARRAY::$my_array"
+echo "::set-env name=COMMIT_TITLE::${my_array[0]}"
+echo "::set-env name=COMMIT_URL::${my_array[1]}"
+echo "::set-env name=COMMIT_CONTENT::${my_array[2]}"
